c310c3b26b90a1bc1cba08f9a9ad6fed7993fc3a
Jonathan <69087307+jonathanmarinho@users.noreply.github.com>
2022-02-16 21:38:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function testeAberto(array=[], valor){
     }
 }
 
-testeAberto([1,2,3,4,5,6,7,8,9], 10);
+testeAberto([1,2,3,4,5,6,7,8,9], 10); // Valores do array e Valor alvo
 // testeAberto([3,8,9,6,8,5], 10);
 // testeAberto([4,8,6,1,3,7], 7);
